d53fa9c9fe9ebcab8a7273cdee5139e53a44d1a3
lilbeb <orangeee04@gmail.com>
2024-02-02 12:06:55 +0500

    Initial Commit

---
+++ foodDeliveryApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  foodDeliveryApp
+//
+//  Created by Элина Борисова on 02.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
